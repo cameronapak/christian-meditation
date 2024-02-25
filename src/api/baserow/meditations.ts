615c0b24292a37fd6108518519fb0573d3821fec
Cameron Pak <cameronandrewpak@gmail.com>
2024-02-25 07:18:23 -0600

    added ability to filter by favorites

--- src/api/baserow/meditations.ts
+++ src/api/baserow/meditations.ts
@@ -17,6 +17,7 @@ export interface Result {
   Duration: Duration
   "YouTube Video ID": string
   "Thumbnail URL": string
+  "Meditation Id": string | number
 }
 
 export interface Creator {
@@ -44,7 +45,7 @@ export async function getAllMeditations({
 
   if (creatorId) {
     const filter = { "filter_type": "AND", "filters": [{ "type": "link_row_has", "field": "Creator", "value": creatorId }], "groups": [] };
-    baseUrl.searchParams.set('creator', encodeURI(JSON.stringify(filter)));
+    baseUrl.searchParams.set('creator', encodeURIComponent(JSON.stringify(filter)));
   }
 
   const meditations: MeditationData = await fetchAndCache({
@@ -69,7 +70,7 @@ export async function getMeditationsByCreatorId(id: number): Promise<MeditationD
   const baseUrl = new URL('https://api.baserow.io/api/database/rows/table/259238/?user_field_names=true');
 
   const filter = { "filter_type": "AND", "filters": [{ "type": "link_row_has", "field": "Creator", "value": id }], "groups": [] };
-  baseUrl.searchParams.set('creator', encodeURI(JSON.stringify(filter)));
+  baseUrl.searchParams.set('user_field_names', encodeURIComponent(JSON.stringify(filter)));
 
   const meditations: MeditationData = await fetchAndCache({
     url: baseUrl.href,
@@ -98,4 +99,52 @@ export async function getMeditationById(id: string): Promise<Result> {
   });
 
   return meditation;
+}
+
+export async function getMeditationsByIds(ids: number[]): Promise<MeditationData> {
+  // const baseUrl = new URL('https://api.baserow.io/api/database/rows/table/259238/?user_field_names=true');
+
+  // const filters = { "filter_type": "OR", "filters": ids.map(id => ({ "type": "equal", "field": "Meditation Id", "value": `${id}` })), "groups": [] };
+  // if (ids.length === 1) {
+  //   filters['filter_type'] = "AND";
+  // }
+  // baseUrl.searchParams.set('filters', encodeURIComponent(JSON.stringify(filters)));
+
+  // try {
+  //   const meditations: MeditationData = await fetchAndCache({
+  //     url: baseUrl.href,
+  //     cacheKey: `meditations-${ids.join('-')}`,
+  //     options: {
+  //       method: "GET",
+  //       headers: {
+  //         Authorization: 'Token ' + import.meta.env.BASEROW_API_KEY,
+  //       }
+  //     }
+  //   })
+
+  //   // @ts-ignore - Sometimes the error exists.
+  //   if (meditations?.error) {
+  //     // @ts-ignore - Sometimes the error exists.
+  //     throw new Error(meditations?.detail?.error || 'Failed to fetch meditations');
+  //   }
+
+  //   return meditations
+  // } catch (error) {
+  //   console.error(error);
+  //   throw error;
+  // }
+
+  console.warn('TODO for Cam - Figure out how to get meditations by ids using standard filter to save API calls.');
+
+  const results = [];
+  for (let i = 0; i < ids.length; i++) {
+    const id = ids[i];
+    const meditation = await getMeditationById(id.toString());
+
+    if (meditation) {
+      results.push(meditation);
+    }
+  }
+
+  return { results } as MeditationData;
 }
